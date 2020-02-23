d819411cd71517c03809b4dfb6f5a7b83ba19d72
shashankyadav99 <shashank.yadav_cs18@gla.ac.in>
2020-02-23 19:10:56 +0530

    Completed Definition of Getter and Setter.

---
+++ src/node/Node.java
@@ -0,0 +1,22 @@
+package node;
+
+public class Node<node> {
+    private node data;
+    private Node<node> next;
+
+    public node getData() {
+        return data;
+    }
+
+    public void setData(node data) {
+        this.data = data;
+    }
+
+    public Node<node> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<node> next) {
+        this.next = next;
+    }
+}

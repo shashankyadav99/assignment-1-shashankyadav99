00218ce8f1fc28c415f290f18ef3360dd24edd3e
shashankyadav99 <shashank.yadav_cs18@gla.ac.in>
2020-02-23 18:47:44 +0530

    Completed Definition of Getter & Setter.

---
+++ src/person/Person.java
@@ -0,0 +1,40 @@
+package person;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String emailID;
+    private StringBuffer phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public StringBuffer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(StringBuffer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

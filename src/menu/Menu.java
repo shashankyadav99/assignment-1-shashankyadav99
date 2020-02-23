51a8bb3322ba583e93f3f2aafe2c7d40c329b1ec
shashankyadav99 <shashank.yadav_cs18@gla.ac.in>
2020-02-23 19:53:48 +0530

    Completed Definition of Constructor.

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,7 +1,16 @@
 package menu;
 
+import dataStructure.MyLinkedList;
 import person.Person;
+
+import java.util.Scanner;
 
 public class Menu {
     private MyLinkedList<Person> linkedList;
+    private Scanner scan;
+
+    public Menu() {
+        scan = new Scanner(System.in);
+        linkedList = new MyLinkedList<>();
+    }
 }
